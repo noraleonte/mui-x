a375e6fe888aec20881af431fd0bc3e26b171ae8
delangle <flaviendelangle@gmail.com>
2023-11-08 17:29:51 +0100

    Work

--- packages/x-date-pickers/src/internals/components/FakeTextField/FakeTextField.tsx
+++ packages/x-date-pickers/src/internals/components/FakeTextField/FakeTextField.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 import { visuallyHidden } from '@mui/utils';
 import useForkRef from '@mui/utils/useForkRef';
